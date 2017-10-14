cca452a5ddab47f6fe2d4371d6acc686cc39292c
lili <liliyoucomputer@gmail.com>
2017-10-14 17:40:47 +0800

    [LeakCanary] Complete setting LeakCanary.

--- app/src/main/java/com/practice/advanced/MyApp.kt
+++ app/src/main/java/com/practice/advanced/MyApp.kt
@@ -5,15 +5,27 @@ import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
 
 
-
 /**
  * Created by xuyating on 2017/9/30.
  */
 class MyApp : Application() {
 
-    private var mRefWatcher: RefWatcher? = null
+
+    companion object {
+        private var instance: Application? = null
+        private var refWatcher: RefWatcher? = null
+        fun instance() = instance!!
+        fun refWatcher() = refWatcher!!
+    }
+
     override fun onCreate() {
         super.onCreate()
-        mRefWatcher = LeakCanary.install(this)
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        instance = this
+        refWatcher = LeakCanary.install(this)
     }
+
 }
